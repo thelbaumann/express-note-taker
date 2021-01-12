6786e365ce80e692c413af708defd2eb3c10c61a
lbmann <l.bmann@yahoo.com>
2021-01-12 16:48:05 -0500

    changing folder strucutre to try to eliminate errors

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log(`listening on ${PORT}...`);
